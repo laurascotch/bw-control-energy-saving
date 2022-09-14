c037b04bc06f33bc4e67d87efdd745219f7e6697
Laura Scoccianti <laura.scoccianti@tndigit.it>
2022-09-14 12:23:34 +0200

    integrato how to run, aggiunti commenti codice get_graph

--- HOW_TO_RUN.md
+++ HOW_TO_RUN.md
@@ -24,11 +24,16 @@ Make sure that `start_ryu.sh` is executable.
 #### Initial setup
 | Where | What to do |
 |---|---|
-| VM | Run the network simulation<br>`cd ~/comnetsemu`<br>`sudo python3 mesh.py`<br>Wait for the mininet shell to appear |
-| VM | Try a `pingall` in the mininet shell |
+| VM | Run the network simulation (whatever topology you want)<br>`cd ~/comnetsemu`<br>`sudo python3 mesh.py`<br>Wait for the mininet shell to appear |
 | Host | In a browser, check that `http://localhost:8080/stats/switches` returns a list of 5 switches |
+| Host | In a browser, check that `http://localhost:8080` shows the topology |
+| Host | Run `python3 utilities/get_graph.py` which will disable redundant links (breaking the loops in the topology) |
+| Host | Wait up to 2 minutes. By refreshing the web page of the topology, you should see it changing |
+| Host | In a browser, check that `http://localhost:8080/stats/flow/<switch_id>` shows three flows |
+| VM | Try a `pingall` in the mininet shell |
+| Host | In a browser, check that `http://localhost:8080/stats/flow/<switch_id>` shows a populated flow table |
 | Host | Run `python3 initial_setup.py` which can be found in the repo |
-| Host | Now check that all the switches' interfaces are working at 10Mbps rate by running `python3 get_port_work_info.py` |
+| Host | Now check that all the switches' interfaces are working at 10Mbps rate by running `python3 checks/get_port_work_info.py` |
 
 #### You are finally ready to run the energy optimization script
 
@@ -48,6 +53,18 @@ Wanna test how the network behaves when the server `srv` transmits 2GB of data t
 
 In the terminal where `energy.py` is running, you should notice that quite a few interfaces are working and that the power usage of some switches has changed. 
 In the meanwhile, in the iperf statistics shown in the mininet CLI, you should notice the connection speed increasing to allow the transmission to finish in as few seconds as possible.
+
+#### Changing topology
+Upon activating/deactivating links, you need to recalculate the spanning tree of the network in order to break unwanted loops.
+
+| Where | What to do |
+|---|---|
+| Host | Check the topology at `http://localhost:8080` |
+| Host | Run `python3 utilities/get_graph.py` which will break the loops in the topology |
+| Host | Wait up to 2 minutes. By refreshing the web page of the topology, you should see it changing |
+| Host | Clear the flow table of all the switches by running: `python3 utilities/clean_flows.py` |
+| VM | Try a `pingall` in the mininet shell |
+| Host | In a browser, check that `http://localhost:8080/stats/flow/<switch_id>` shows a populated flow table |
 
 ### End of the experiment
 
--- utilities/get_graph.py
+++ utilities/get_graph.py
@@ -68,7 +68,6 @@ def get_all_switches():
 
 
 def shut_links(links,new_links):
-    to_be_shut = []
     for link in new_links:
         link_str = f"s{link[0]},s{link[1]}"
         link_str_reverse = f"s{link[1]},s{link[0]}"
@@ -80,15 +79,12 @@ def shut_links(links,new_links):
         port = ports.split(',')
         s0 = re.match("s(\d+)?-eth(\d+)?",port[0])
         s1 = re.match("s(\d+)?-eth(\d+)?",port[1])
-        dpid0 = s0[1]#.rjust(16, '0')
-        portno0 = s0[2]
-        dpid1 = s1[1]#.rjust(16, '0')
-        portno1 = s1[2]
+        dpid0 = s0[1]
+        portno0 = s0[2]     # we just need to disable one side of the link because links are "double" (e.g. we have both s1,s2 and s2,s1)
         url = f"http://localhost:8080/stats/portdesc/modify"
 
         data0 = json.dumps({"dpid": dpid0, "port_no":portno0, "config": 1, "mask": 101})   # mask 101 per non avere problemi. config 1 shutta, config 0 unshutta
-        #data1 = json.dumps({"dpid": dpid1, "port_no":portno1, "config": 1, "mask": 101})   # mask 101 per non avere problemi. config 1 shutta, config 0 unshutta
-
+        
         crl = pycurl.Curl()
         crl.setopt(pycurl.POST, 1)
         crl.setopt(crl.URL, url)
@@ -97,43 +93,31 @@ def shut_links(links,new_links):
 
         crl.perform()
         crl.close()
-        '''
-        crl = pycurl.Curl()
-        crl.setopt(pycurl.POST, 1)
-        crl.setopt(crl.URL, url)
-        crl.setopt(crl.POSTFIELDS, data1)
-        crl.setopt(crl.VERBOSE, 1)
-
-        crl.perform()
-        crl.close()
-        '''
 
 
 if __name__ == "__main__":
+    # ========== get switches and hosts from controller ==========
     switches = get_all_switches()
     switches.sort()
     sw_hosts, host_per_switch = get_all_hosts(switches)
+    # ==============================
 
-    # add nodes
+    # ========== add nodes (switches) to the graph ==========
     for s in switches:
         G.add_node(s)
+    # ==============================
 
-    # get links from controller
+    # ========== get links from controller ==========
     b_obj = BytesIO()
     crl = pycurl.Curl()
-
     crl.setopt(crl.URL, 'http://localhost:8080/v1.0/topology/links')
     crl.setopt(crl.WRITEDATA, b_obj)
-
     crl.perform()
     crl.close()
 
     get_body = b_obj.getvalue()
-
     raw_data = json.loads(get_body.decode('utf8'))
-
-    print(raw_data)
-
+    
     links = {}  # here we save which ports create the link in the form of: 's1,s2': 's1-eth1,s2-eth1'
 
     for entry in raw_data:
@@ -144,16 +128,22 @@ if __name__ == "__main__":
         G.add_edge(s1,s2)
         edge = f"s{s1},s{s2}"
         links[edge] = f"{p1},{p2}"
+    # ==============================
 
-    #print(links)
+    # ========== build a tree from the graphs so to break the loops ==========
+    # each node/switch is given a value based on the connections to other switches
+    # and number of connected hosts. The bfs tree will start from the node with the
+    # highest score. This way, it is more likely that switches with no hosts connected
+    # will become leaves. Those switches can be shut down since no traffic will ever
+    # need to pass there.
     bfs_value = {}
     for s in switches:
         s_degree = G.degree[s]
         bfs_value[s] = s_degree * 2 + host_per_switch[s]
     bfs_origin = max(bfs_value, key=bfs_value.get)
 
+    # "optimized spanning tree"
     T = nx.bfs_tree(G,bfs_origin)
-    #new_links = [line for line in nx.generate_edgelist(T, data=False)] 
     new_links = list(T.edges())
 
     # shut unwanted links
@@ -162,10 +152,10 @@ if __name__ == "__main__":
     # if a leaf switch has no hosts attached, it can be turned off
     # let's find them:
     switch_off = [x for x in T.nodes() if T.out_degree(x)==0 and host_per_switch[x]==0]
+    # to do: disable all ports of the found switches
+    # ==============================
 
-    #print(sorted(T.edges()))
-
-    # now let's add hosts to the bfs_tree
+    # ========== populate the tree graph with hosts and compute flows ==========
     hosts = []
     for port,host in sw_hosts.items():
         m = re.match("s(\d+)?-eth(\d+)?",port)
@@ -181,6 +171,6 @@ if __name__ == "__main__":
     # now we can compute paths between each other host
     for host in hosts:
         paths = nx.single_source_shortest_path(network,host)
-        print(paths)    # must delete paths to switches and maintain only those to hosts
-        
+        # must delete paths to switches and maintain only those to hosts
         # after having all paths, we can populate the ryu flow tables for each switch
+    # ==============================
