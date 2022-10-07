a16101f6acab5499bf32cbdb5a97b71c276f4c84
Laura Scoccianti <laura.scoccianti@gmail.com>
2022-10-07 20:53:29 +0200

    script per runnare comandi in mininet in automatico, per avere uniformità nelle simulazioni

---
+++ auto_traffic_emulator.py
@@ -0,0 +1,73 @@
+# this script is necessary in order to run the SAME simulation (in terms of traffic flows)
+# it focuses the mininet emulator window and inputs the commands to run iperf
+# and other connections between hosts in the network
+
+import pygetwindow as gw
+import pyautogui as pag
+from pyautogui import press, typewrite, hotkey
+import pydirectinput
+import time
+import os
+
+
+def ampersand(): #pydirectinput non conosce &
+    pydirectinput.keyDown('shift')
+    pydirectinput.press('6')
+    pydirectinput.keyUp('shift')
+
+
+def mininet_cmd(command):
+    # pydirectinput inserisce input da tastiera inglese, quindi quello che per lui è /, per me è -
+    command = command.replace('-', '/')
+    
+    for i in range(len(command)):
+        if command[i].isupper():
+            pydirectinput.keyDown('shift')
+            pydirectinput.press(command[i].lower())
+            pydirectinput.keyUp('shift')
+            continue
+        if command[i] == '&':
+            ampersand()
+            continue
+        pydirectinput.press(command[i])
+
+    pydirectinput.press('enter')
+
+
+pydirectinput.PAUSE=0.01
+
+
+win = gw.getWindowsWithTitle('comnetsemu')[0]
+try:
+    win.activate()
+except gw.PyGetWindowException:
+    pass
+
+time.sleep(1)
+pydirectinput.press('enter')
+
+
+time.sleep(4)
+mininet_cmd("h1 iperf -c 10.0.0.100 -n 10M -i2 &")
+
+
+time.sleep(1)
+mininet_cmd("srv iperf -c 10.0.0.1 -n 1500M -i2 &")
+
+time.sleep(8)
+
+mininet_cmd("h2 iperf -c 10.0.0.4 -n 1000M -i2 &")
+time.sleep(4)
+mininet_cmd("h7 iperf -c 10.0.0.3 -n 800M -i2 &")
+
+time.sleep(20)
+
+mininet_cmd("h1 iperf -c 10.0.0.3 -n 10M -i2 &")
+time.sleep(1)
+mininet_cmd("h3 iperf -c 10.0.0.1 -n 500M -i2")
+
+time.sleep(34)
+
+mininet_cmd("h2 iperf -c 10.0.0.6 -n 10M -i2 &")
+time.sleep(1)
+mininet_cmd("h6 iperf -c 10.0.0.2 -n 850M -i2 &")
