c0c1018478b700da5c6a10c222069d6646eaa1a7
Laura Scoccianti <laura.scoccianti@gmail.com>
2022-10-18 20:30:51 +0200

    nuovo pattern traffico per test

--- run_project.py
+++ run_project.py
@@ -21,13 +21,13 @@ power_per_intf = {'0.0':0, '10.0':0.1, '100.0':0.2, '1000.0':0.5, '10000.0':5.0}
 BASE_POWER = 20
 used_ports = {}     # keeps track of whether a packet flow is going through a certain port over time
 
-OUTPUT_NAME = "221016_01_"
+OUTPUT_NAME = "221016_R01_04_"
 
 INITIAL_SPEED = 10
 SENSITIVITY = 67500 # bytes per time unit that triggers the sensing of port usage - 1,250,000 is the Bps of 10Mbps, 125000 is the Bps of 1Mbps
 ADAPTIVE_BITRATE = True # True per run ottimizzata
 DISABLE_UNUSED = True # True per run ottimizzata
-MAX_10G = True
+MAX_10G = False
 ANALYSIS_DURATION = 60
 
 DEBUG_LOG = False
--- shpy.py
+++ shpy.py
@@ -1,3 +1,4 @@
+'''
 py time.sleep(4)
 py net['h1'].cmd("iperf -c 10.0.0.100 -n 10M -i2 &")
 py time.sleep(1)
@@ -32,3 +33,25 @@ py time.sleep(1)
 py net['h3'].cmd("iperf -c 10.0.0.1 -n 250M -i2 &")
 py time.sleep(1)
 py net['srv'].cmd("iperf -c 10.0.0.5 -n 2500M -i2 &")
+'''
+
+# CASO PARTICOLARE TOPOLOGIA RING/LINEARE
+
+py time.sleep(4)
+py net['srv'].cmd("iperf -c 10.0.0.7 -n 2000M -i2 &")
+py time.sleep(1)
+py net['h6'].cmd("iperf -c 10.0.0.100 -n 1500M -i2 &")
+py time.sleep(1)
+py time.sleep(8)
+py time.sleep(1)
+py time.sleep(4)
+py net['h5'].cmd("iperf -c 10.0.0.100 -n 800M -i2 &")
+py time.sleep(17)
+py net['srv'].cmd("iperf -c 10.0.0.6 -n 2500M -i2 &")
+py time.sleep(5)
+py net['srv'].cmd("iperf -c 10.0.0.5 -n 1500M -i2 &")
+py time.sleep(20)
+py net['h6'].cmd("iperf -c 10.0.0.100 -n 800M -i2 &")
+py time.sleep(3)
+py net['h5'].cmd("iperf -c 10.0.0.100 -n 250M -i2 &")
+py time.sleep(1)
